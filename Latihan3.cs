31485b62b20f17fb3b9c22da742ce3f9030ebf59
sidiqpangestu30 <sidiqpangestu30@gmail.com>
2020-09-22 09:50:51 +0700

    Add files via upload

---
+++ Latihan3.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan3 : MonoBehaviour {
+	/// <summary>
+	/// Merupakan fungsi penjumlahan dari 2 input angka
+	/// </summary>
+	/// <param name="a">Masukkan Angka Integer</param>
+	/// <param name="b">Masukkan Angka Integer</param>
+	int Jumlah(int a,int b)
+	{
+		return a + b;
+	}
+	// Use this for initialization
+	void Start () {
+		
+		int c = Jumlah (60, 40);
+		Debug.Log ("Hasil Jumlah a dan b adalah " + c);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
