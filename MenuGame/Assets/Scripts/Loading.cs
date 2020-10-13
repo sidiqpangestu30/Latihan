93e8f7fe30618e549b546e47ef2995cfa05dbdcb
sidiqpangestu30 <sidiqpangestu30@gmail.com>
2020-10-13 13:15:54 +0700

    Add files via upload

---
+++ MenuGame/Assets/Scripts/Loading.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour {
+	[SerializeField] private float delayLoading = 3f;
+	[SerializeField] private string namaScene;
+	private float timeElapsed;
+	// Use this for initialization
+	
+	// Update is called once per frame
+	private void Update () {
+		timeElapsed += Time.deltaTime;
+		if (timeElapsed > delayLoading) {
+			SceneManager.LoadScene (namaScene);
+		}
+	}
+}
---
+++ MenuGame/Assets/Scripts/Scene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene : MonoBehaviour {
+
+	public void MenuScene(){
+		SceneManager.LoadScene ("mainMenu");
+	}
+	public void LoadingScene(){
+		SceneManager.LoadScene ("Loading");
+	}
+	public void KeluarScene(){
+		Application.Quit ();
+	}
+}
