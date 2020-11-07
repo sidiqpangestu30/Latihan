52aaf907d7399f1ac9c87e247d1a34bb668a7d59
sidiqpangestu30 <sidiqpangestu30@gmail.com>
2020-11-07 21:20:49 +0700

    Add files via upload

---
+++ PingPong2/Scripts/PaddlerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddlerController : MonoBehaviour
+{
+    public float batasAtas;
+    public float batasBawah;
+    public float kecepatan;
+    public string axis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float gerak=Input.GetAxis(axis)*kecepatan*Time.deltaTime;
+        //transform.Translate(0,gerak,0);
+        float nextPos=transform.position.y+gerak;
+        if(nextPos>batasAtas){
+            gerak=0;
+        }if(nextPos<batasBawah){gerak=0;}
+        transform.Translate(0,gerak,0);
+    }
+}
